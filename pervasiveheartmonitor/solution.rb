8decedd7d82c2832d4a61c840a68fe6ff96281ec
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-05-29 16:51:20 -0300

    added pervasiveheartmonitor solution in ruby.

---
+++ pervasiveheartmonitor/solution.rb
@@ -0,0 +1,14 @@
+STDIN.each_line do |line|
+    values = line.split
+    names = []
+    sum = []
+    output = 0
+
+    values.each do |value|
+      value.downcase.match?("[a-z]") ? names << value : sum << value
+    end
+
+    output = sum.map(&:to_f).sum / sum.size
+    
+    puts "%0.6f #{names.join(" ")}" % [output]
+end
