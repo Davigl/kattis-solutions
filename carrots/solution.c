5105978de00902b9cbd7de1dfd46ce54c0b25d2e
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-05-29 15:53:29 -0300

    added carrots solution in c.

---
+++ carrots/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x,y,i;
+    char nome[100];
+
+    scanf("%d",&x);
+    scanf("%d",&y);
+
+    for(i = 0; i < x; i ++) {
+        scanf("%s",&nome);
+    } printf("%d",y);
+}
