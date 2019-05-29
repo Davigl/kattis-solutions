2946c59d95fedbf169a31250cd856d2094880e3c
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-05-29 16:09:19 -0300

    added apaxiaaans solution in java.

---
+++ apaxiaaans/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		String word, output = "";
+
+		word = ler.nextLine();
+		
+		if (word.length() == 1) {
+			output += word.charAt(0);
+		} else {
+			for (int i = 0; i < word.length() - 1; i++) {
+				if (word.charAt(i) != word.charAt(i + 1)) {
+					output += word.charAt(i);
+				}
+			}
+			output += word.charAt(word.length() - 1);
+		}
+
+		System.out.println(output);
+	}
+
+}
