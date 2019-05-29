40b4363a12a62d8a1edd85a7231dd34c3fafd49e
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-05-29 15:50:50 -0300

    added timeloop solution in c.

---
+++ timeloop/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i,qtd;
+
+    scanf("%d",&qtd);
+
+    for(i = 1; i <= qtd; i++) {
+        printf("%d Abracadabra\n",i);
+    }
+}
+
