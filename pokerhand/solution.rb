84003d7f72f9bb4c9c33fb58509fe358358a9ee6
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 14:38:50 -0300

    update pokerhand solution in ruby.

--- pokerhand/solution.rb
+++ pokerhand/solution.rb
@@ -2,13 +2,13 @@ input = gets.chomp.split
 suit = {}
 
 input.each do |value|
-	unless suit.include?(value[0].to_sym)
+	unless suit.include? (value[0].to_sym)
 		suit[value[0].to_sym] = 1
 	else
 		suit[value[0].to_sym] += 1
 	end
 end
 
-max_hand = suit.max_by {|x, v| v}.to_a[1]
+max_hand = suit.max_by { |x, v| v }.to_a[1]
 
 puts max_hand
