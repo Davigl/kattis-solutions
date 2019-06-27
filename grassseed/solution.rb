9bd983321f7a6e951bbb9f26aa64d93db2c86eee
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 02:46:46 -0300

    update grassseed in ruby.

--- grassseed/solution.rb
+++ grassseed/solution.rb
@@ -1,9 +1,9 @@
 cost = gets.chomp.to_f
-qt = gets.chomp.to_i
+quantity = gets.chomp.to_i
 sum = 0
 
-qt.times do |i|
-  x,y = gets.chomp.split.map(&:to_f)
+quantity.times do |i|
+  x, y = gets.chomp.split.map(&:to_f)
 
   sum += (x * y) * cost
 end
