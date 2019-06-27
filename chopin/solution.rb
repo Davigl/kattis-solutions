994b34322b9995e56dac59a293e1e96acb4d8cfe
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 02:36:54 -0300

    update chopin solution in ruby.

--- chopin/solution.rb
+++ chopin/solution.rb
@@ -1,24 +1,24 @@
 scale = {Aq: "Bb", Cq: "Db", Dq: "Eb", Fq: "Gb", Gq: "Ab"}
 scale_2 = {Bb: "Aq", Db: "Cq", Eb: "Dq", Gb: "Fq", Ab: "Gq"}
-cont = 1
+count = 1
 
 STDIN.each_line do |line|
     input = line.split
     
-    note = input[0]
+    note = input.first
     note = note.gsub("#", "q")
     
     if scale.has_key? (note.to_sym) or scale_2.has_key? (note.to_sym)
         if scale.has_key? (note.to_sym)
             output = scale[note.to_sym].gsub("q", "#")
-            puts "Case #{cont}: #{output} #{input[1]}"
+            puts "Case #{count}: #{output} #{input[1]}"
         else
             output = scale_2[note.to_sym].gsub("q", "#")
-            puts "Case #{cont}: #{output} #{input[1]}"
+            puts "Case #{count}: #{output} #{input[1]}"
         end
     else
-        puts "Case #{cont}: UNIQUE"
+        puts "Case #{count}: UNIQUE"
     end
     
-    cont += 1
+    count += 1
 end
