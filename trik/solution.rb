0772b8c93767afe0940145fe142b7c8bac17839b
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 13:42:11 -0300

    update trik solution in ruby.

--- trik/solution.rb
+++ trik/solution.rb
@@ -2,17 +2,17 @@ input = gets.chomp
 cup = 1
 
 input.chars.each do |c|
-	if c == "A" and cup == 1
+	if c.eql? "A" and cup.eql? 1
 		cup = 2
-	elsif c == "A" and cup == 2
+	elsif c.eql? "A" and cup.eql? 2
 		cup = 1
-	elsif c == "B" and cup == 2
+	elsif c.eql? "B" and cup.eql? 2
 		cup = 3
-	elsif c == "B" and cup == 3
+	elsif c.eql? "B" and cup.eql? 3
 		cup = 2
-	elsif c == "C" and cup == 3
+	elsif c.eql? "C" and cup.eql? 3
 		cup = 1
-	elsif c == "C" and cup == 1
+	elsif c.eql? "C" and cup.eql? 1
 		cup = 3
 	end
 end
