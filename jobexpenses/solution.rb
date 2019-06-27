cdf58d313b77c934c728194fa9b890db927a21cd
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 14:54:43 -0300

    update jobexpenses solution in ruby.

--- jobexpenses/solution.rb
+++ jobexpenses/solution.rb
@@ -1,8 +1,7 @@
-n = gets.chomp.to_i
+cases = gets.chomp.to_i
+values = gets.chomp.split.map(&:to_i)
 expenses = 0
 
-values = gets.chomp.split.map(&:to_i)
-
-values.each {|v| expenses += v if v < 0 }
+values.each { |value| expenses += value if value < 0 }
 
 puts expenses.abs
