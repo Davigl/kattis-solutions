f4f737fe8cbdcc947fc00a8933cc043530419bdd
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 15:02:19 -0300

    update hidden solution in ruby.

--- hidden/solution.rb
+++ hidden/solution.rb
@@ -1,23 +1,22 @@
 input = gets.chomp.split
-
 password, word = input[0].chars, input[1].chars
-flag = false
-
 verify = []
+
+flag = false
 
 word.each do |char|
 	if password.include? char
 		if verify.include? char
 			if verify.count(char) < password.count(char)
-				verify << char
+				verify.push(char)
 			end
 		else
-			verify << char
+			verify.push(char)
 		end
 	end
 end
 
-if verify[0..password.size-1] == password
+if verify[0..password.size-1].eql? password
 	flag = true
 end
 
