b685375b78f3d87ae25c3ac46f0e3c0bf0b2a196
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 02:30:41 -0300

    update batterup solution in ruby.

--- batterup/solution.rb
+++ batterup/solution.rb
@@ -1,8 +1,9 @@
 x = gets.chomp.to_i
 y = gets.chomp.split.map(&:to_i)
+
 output = 0
 
-y.each { |value| value == -1 ? x -= 1 : output += value }
+y.each { |value| value.eql? -1 ? x -= 1 : output += value }
 
 output = output.to_f / x.to_f
 
