6cd0bff16a40230905102310c34ff58177d028e0
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 14:29:17 -0300

    update shatteredcake solution in ruby.

--- shatteredcake/shatteredcake.rb
+++ shatteredcake/shatteredcake.rb
@@ -1,10 +1,10 @@
-x = gets.chomp.to_i
+width_cake = gets.chomp.to_i
 slices = gets.chomp.to_i
 sum = 0
 
 slices.times do |i|
-	input = gets.chomp.split.map(&:to_i)
-	sum += input[0] * input[1]
+	width, length = gets.chomp.split.map(&:to_i)
+	sum += width * length
 end
 
-puts sum/x
+puts sum/width_cake
