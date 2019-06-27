06960982458a5b8b815d5bb731f920702cddc9c2
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 14:25:30 -0300

    update sibice solution in ruby.

--- sibice/solution.rb
+++ sibice/solution.rb
@@ -1,15 +1,9 @@
-input = gets.chomp.split.map(&:to_i)
-
-n, x, y = input[0], input[1], input[2]
+n, x, y = gets.chomp.split.map(&:to_i)
 
 diagonal = Math.sqrt((x ** 2) + (y ** 2)).to_i
 
 n.times do |i|
 	line = gets.chomp.to_i
 	
-	if line <= diagonal
-		puts "DA"
-	else
-		puts "NE"
-	end
+	puts line <= diagonal ? "DA" : "NE"
 end
