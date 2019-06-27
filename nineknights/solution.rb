d9ee085a53a158ebf4b60ebc6b84e1487f9f60d2
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 14:48:30 -0300

    update nineknights solution in ruby.

--- nineknights/solution.rb
+++ nineknights/solution.rb
@@ -1,68 +1,68 @@
-x = []
+values = []
 
 5.times do |i|
   word = gets.chomp.split('')
-  x << word
+  values.push(word)
 end
 
 flag = false
 count_k = 0
 
-x.each_with_index do |line, j|
+values.each_with_index do |line, j|
   count_k += line.count('k')
   line.each_with_index do |value, i|
-    if value == 'k'
+    if value.eql? 'k'
       if (j + 1 < 5 and i + 2 < 5)
-        if (x[j + 1][i + 2] == 'k')
+        if (x[j + 1][i + 2].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j + 1 < 5 and i - 2 >= 0)
-        if (x[j + 1][i - 2] == 'k')
+        if (x[j + 1][i - 2].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j - 1 >= 0 and i + 2 < 5)
-        if (x[j - 1][i + 2] == 'k')
+        if (x[j - 1][i + 2].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j - 1 >= 0 and i - 2 >= 0)
-        if (x[j - 1][i - 2] == 'k')
+        if (x[j - 1][i - 2].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j - 2 >= 0 and i - 1 >= 0)
-        if (x[j - 2][i - 1] == 'k')
+        if (x[j - 2][i - 1].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j + 2 < 5 and i - 1 >= 0)
-        if (x[j + 2][i - 1] == 'k')
+        if (x[j + 2][i - 1].eql? 'k')
           flag = true
           break
         end
       end
       
       if (j - 2 >= 0 and i + 1 < 5)
-        if (x[j - 2][i + 1] == 'k')
+        if (x[j - 2][i + 1].eql? 'k')
           flag = true
           break
         end
       end
 
       if (j + 2 < 5 and i + 1 < 5)
-        if (x[j + 2][i + 1] == 'k')
+        if (x[j + 2][i + 1].eql? 'k')
           flag = true
           break
         end
