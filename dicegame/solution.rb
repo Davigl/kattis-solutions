bb85cb7579840c31d999717654bf78cbe202720d
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 15:11:56 -0300

    update dicegame solution in ruby.

--- dicegame/solution.rb
+++ dicegame/solution.rb
@@ -4,10 +4,4 @@ gunnar = gets.chomp.split.map(&:to_i)
 sum_emma = emma.inject(:+)
 sum_gunnar = gunnar.inject(:+)
 
-if sum_emma > sum_gunnar
-	puts "Gunnar"
-elsif sum_gunnar > sum_emma
-	puts "Emma"
-else
-	puts "Tie"
-end
+puts sum_emma > sum_gunnar ? "Gunnar" : sum_gunnar > sum_emma ? "Emma" : "Tie"
