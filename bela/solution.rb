6e70e8ee12c82ca21bc1be1512b3c5b7d267006d
Davi <davi.guimaraesleite@gmail.com>
2019-06-27 01:45:41 -0300

    added bela solution in ruby.

---
+++ bela/solution.rb
@@ -0,0 +1,17 @@
+input = gets.chomp.split
+dominant = input[1]
+hands = input[0].to_i * 4
+output = 0
+
+set = {"A": [11,11], "K": [4,4], "Q": [3,3], "J": [20,2], "T": [10,10], "9": [14,0], "8": [0,0], "7": [0,0]}
+
+hands.times do |i|
+	card = gets.chomp
+	if card[1] == dominant
+		output += set[(card[0].to_sym)][0]
+	else
+		output += set[card[0].to_sym][1]
+	end
+end
+
+puts output
