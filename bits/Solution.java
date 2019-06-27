47f68493c3d8f1987e6d4dd33238a8af7273de5e
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-27 03:12:13 -0300

    update bits solution in java.

--- bits/Solution.java
+++ bits/Solution.java
@@ -4,32 +4,32 @@ public class Solution {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
-        int qtd = Integer.parseInt(ler.nextLine());
-        int cont,maior;
-        String binary,num,analisa;
+        int input = Integer.parseInt(ler.nextLine());
+        int count, bigger;
+        String binary, number, temporary;
         
-        for (int i = 0; i < qtd; i++) {
-            maior = 0;
-            cont = 0;
-            analisa = "";
+        for (int i = 0; i < input; i++) {
+            bigger = 0;
+            count = 0;
+            temporary = "";
             num = ler.nextLine();
             
             for (int j = 0; j < num.length(); j++) {
-                analisa += num.charAt(j);
-                binary = Long.toBinaryString(Long.parseLong(analisa));
+                temporary += num.charAt(j);
+                binary = Long.toBinaryString(Long.parseLong(temporary));
 
                 for (int k = 0; k < binary.length(); k++) {
-                    if(binary.charAt(k) == '1') {
-                        cont++;
+                    if (binary.charAt(k) == '1') {
+                        count++;
                     }
                 }
 
-                if (maior <= cont) {
-                    maior = cont;
+                if (bigger <= count) {
+                    bigger = count;
                 } 
 		
-		cont = 0;
-            } System.out.println(maior);
+		count = 0;
+            } System.out.println(bigger);
         }
     }
 }
